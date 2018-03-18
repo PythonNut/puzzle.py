aeee5c591ca07a53ffaec376ebfa99e7f90926f0
PythonNut <PythonNut@users.noreply.github.com>
2018-03-17 21:07:43 -0700

    Add some basic helpers

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ===========
 
 A collection of (potentially) useful functions and definitions to help solve puzzles for [puzzle hunts](https://en.wikipedia.org/wiki/Puzzlehunt).
+
+Note that good programming practice is second to speed for this application.
--- puzzle.py
+++ puzzle.py
@@ -1,7 +1,35 @@
+from string import *
+from functools import *
+from inspect import getargspec
+from itertools import *
+from operator import *
 
+alph = ascii_lowercase
+ALPH = ascii_uppercase
+Alph = ascii_letters
 
+num = digits
+
+p = product
+l = list
+
+def curried(n):
+    def curry(fn):
+        def _inner(*args):
+            if len(args) < n:
+                return curried(n - len(args))(partial(fn, *args))
+            return fn(*args)
+        return _inner
+    return curry
+
+@curried(2)
 def m(*args, **kwargs):
     return list(map(*args, **kwargs))
 
+@curried(2)
 def f(*args, **kwargs):
     return list(filter(*args, **kwargs))
+
+@curried(2)
+def member(objs, obj):
+    return contains(objs, obj)
