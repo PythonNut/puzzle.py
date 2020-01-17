ed06ae292f0714e88b1be84898905565eeb7d117
PythonNut <PythonNut@users.noreply.github.com>
2020-01-17 10:22:19 -0800

    Add VariableDispenser stuff

--- puzzle.py
+++ puzzle.py
@@ -92,3 +92,66 @@ def subsequences(l):
     for length in range(len(l), 0, -1):
         for combination in combinations(l, length):
             yield combination
+
+class IndexConvertor(object):
+    def __init__(self, shape, offset):
+        self.shape = shape
+        self.offset = offset
+
+    def __getitem__(self, i):
+        if len(self.shape) == 1:
+            assert 0 <= i < self.shape[0]
+            return i + self.offset
+
+        return int(np.ravel_multi_index(i, self.shape) + self.offset)
+
+    def __call__(self, j):
+        return np.unravel_index(j - self.offset, self.shape)
+
+    def __str__(self):
+        return f"I{self.shape}"
+
+    __repr__ = __str__
+
+    def size(self):
+        return reduce(op.mul, self.shape)
+
+
+class VariableDispenser(object):
+    def __init__(self):
+        self.offset = 1
+        self.variables = []
+
+    def dispense(self, shape):
+        I = IndexConvertor(shape, self.offset)
+        self.offset += I.size()
+        self.variables.append(I)
+        return I
+
+    def __str__(self):
+        return f"VD{self.variables}"
+
+    __repr__ = __str__
+
+    def size(self):
+        return self.offset - 1
+
+    def top_var(self):
+        return self.size()
+
+    def unflatten(self, flat):
+        assert len(flat) <= self.size()
+        results = []
+        i = 0
+        for v in self.variables:
+            A = np.zeros(v.shape, np.bool)
+            for _ in range(v.size()):
+                i += 1
+                j = v(i)
+                if i > len(flat):
+                    A[j] = False
+                else:
+                    A[v(i)] = flat[i-1] > 0
+            results.append(A)
+
+        return results
